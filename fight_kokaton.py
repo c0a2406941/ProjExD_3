486de84d8de2b2ef9a41d18f414045c9ee417bc4
C0A24069 <c0a2406941@edu.teu.ac.jp>
2025-12-02 16:31:29 +0900

    コンフリクト修正

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -194,16 +194,18 @@ def main():
                 time.sleep(1)
                 return
         
-        for b, bomb in enumerate(bombs):
-            if beam is not None:
-                if beam.rct.colliderect(bomb.rct):
-                    # ビームが爆弾に当たったらどっちも消す
-                    score.score += 1
-                    beam = None
-                    bombs[b] = None
-                    bird.change_img(6, screen)
-                    pg.display.update()
-        bombs = [bomb for bomb in bombs if bomb is not None]
+        for i, beam in enumerate(beams):
+            for b, bomb in enumerate(bombs):
+                if beam is not None:
+                    if beam.rct.colliderect(bomb.rct):
+                        # ビームが爆弾に当たったらどっちも消す
+                        score.score += 1
+                        beams[i] = None
+                        bombs[b] = None
+                        bird.change_img(6, screen)
+                        pg.display.update()
+            bombs = [bomb for bomb in bombs if bomb is not None]
+        beams = [beam for beam in beams if beam is not None]
 
         key_lst = pg.key.get_pressed()
         bird.update(key_lst, screen)
